145138c6222641a3e63cf8825d1c459cfe53d8f4
wilburrr90 <wilburrr90@gmail.com>
2016-09-10 14:29:07 -0400

    Stable, deployed multi-city with NYC and DC.

--- cityaccess.js
+++ cityaccess.js
@@ -1,96 +1,123 @@
-// Initialize a map with leaflet
-var map = L.map('map', { 
-  center: [40.740693, -74.004536], 
+const centers = {
+  nyc: [40.7128, -74.0059],
+  dc: [38.9072, -77.0369]
+}
+
+const map = L.map('map', { 
+  center: centers.nyc, 
   zoom: 11,
   maxZoom: 18,
+  doubleClickZoom: false,
   inertia: false,
   tap: true
 });
-L.tileLayer(
-  'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', 
+L.tileLayer( 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', 
   {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-  }).addTo(map);
+  })
+  .addTo(map);
   
+const walkCircles = {};
+const stops = {};
+const stationPiesD3 = {};
 let willWalk = mm(0.5);
-let walkCircles = []; 
-let stops = [];
-let marker;
-let markerCircle;
-let stationPiesD3;
+let city = 'nyc';
 
-d3.json('complexesM.json', function(error, mta) {
-  stops = mta.features.map(o => {
-    o.properties.latlng = L.latLng(o.geometry.coordinates.reverse());
-    return o.properties;
-  }); 
-
-  walkCircles = stops.map(o => L.circle(o.latlng, 100, 
+const marker = L.marker(centers.nyc, { 
+    draggable: 'true', 
+    zIndexOffset: 1000,
+    icon: new L.Icon.Default({ shadowSize: [0, 0]})
+  })
+  .on('drag', onMarkerDrag)
+  .on('dragend', updateWalkingCircles)
+  .addTo(map); 
+const markerCircle = L.circle(centers.nyc, mm(0.5), 
+  { 
+    className: 'marker-circle',
+    stroke: false,
+    fillOpacity: 1
+  })
+  .addTo(map);
+  
+function focusMap(newCity) {
+  city = newCity
+  map.panTo(centers[city]);
+  map.setZoom(11);
+  marker.setLatLng(centers[city]);
+  markerCircle.setLatLng(centers[city]);
+  updateWalkingCircles();
+}
+  
+function addWalkCircles(city) {
+  const walkCirclesTemp = stops[city].map(o => L.circle(o.latlng, 100, 
     { 
       className: 'walk-circle', 
       stroke: false,
       fillOpacity: 1
     }));
-  L.featureGroup(walkCircles).addTo(map);
+  L.featureGroup(walkCirclesTemp).addTo(map);
+  return walkCirclesTemp
+}
 
-  marker = L.marker([40.740693, -74.004536], { draggable: 'true' })
-    .addTo(map)
-    .on('drag', onDrag)
-    .on('dragend', updateWalkingCircles); 
-  markerCircle = L.circle([40.740693, -74.004536], mm(0.5), 
-    { 
-      className: 'marker-circle',
-      stroke: false,
-      fillOpacity: 1
-    }).addTo(map);
-
-  const stationPies = stops.map(o => {
-    const popupContent = document.createElement("div");
-    const popupName = document.createElement("h1");
-    const popupLines = o.serves.map(line => { 
-      const temp = new Image(20, 20);
-      temp.src = 'lineImages/' + line + '.png';
-      return temp;
-    })
-    
-    popupName.appendChild(document.createTextNode(o.name));
-    popupContent.appendChild(popupName);
-    popupLines.forEach(l => { popupContent.appendChild(l); })
-    
-    const popup = L.popup({ closeButton: false, autoPan: false })
-      .setLatLng(o.latlng)
-      .setContent(popupContent);
-      
-    const tempIcon = L.divIcon({
-      className: 'stop-icon', 
-      iconSize: 15,
-      html: '<svg class="stop-svg" viewBox="0 0 100 100"></svg>'
+function addStationPies(city) {
+  const stationPies = stops[city].map(o => {
+    const tipContent = document.createElement("div");
+    tipContent.innerHTML = '<h1>' + o.name + '</h1>';
+    o.serves.forEach(line => { 
+      const img = new Image(20, 20);
+      img.src = 'lineImages/' + city + '/'+ line + '.png';
+      tipContent.appendChild(img);
     });
-
-    return L.marker(o.latlng, { icon: tempIcon })
-      .on('mouseover', () => { popup.openOn(map); })
-      .on('mouseout', () => { map.closePopup(); });
+    
+    return L.marker(o.latlng, 
+      { icon: L.divIcon({ className: 'stop-icon' }) })
+      .bindTooltip(L.tooltip().setContent(tipContent));
   })
-  L.featureGroup(stationPies).addTo(map);
-  stationPiesD3 = d3.selectAll('.stop-svg').data(stops);
-  makePies();
-  updateWalkingCircles();
-  //onDrag();
-  onZoom();
-}); // end of d3.json
+  const spg = L.featureGroup(stationPies).addTo(map);
+  return d3.selectAll(spg.getLayers().map(o => o.getElement()))
+    .data(stops[city]).append('svg')
+    .attr('viewBox','0 0 100 100')
+    .attr('width','30%')
+    .attr('class','stop-svg');
+}
 
-function makePies() {
+function makePies(stationPies) {
   const arc = d3.arc()
     .innerRadius(0)
     .outerRadius(50)
     .startAngle(0);    
 
-  stationPiesD3.selectAll('slices')
+  stationPies.selectAll('slices')
     .data(d => d.colors).enter()
     .append('path')
     .attr('transform', 'translate(50,50)')
     .attr('d', (d, i, j) => arc({ endAngle: 2 * Math.PI * (1 - i / j.length)}))
     .attr('fill', d => d)
+}
+
+// Get DC stops
+d3.json('stations/dc.json', function(error, stopsJson) {
+  stops.dc = stopsJson;
+  walkCircles.dc = addWalkCircles('dc');
+  stationPiesD3.dc = addStationPies('dc');
+  makePies(stationPiesD3.dc);
+});
+
+// Get NYC stops
+d3.json('stations/nyc.json', function(error, stopsJson) {
+  stops.nyc = stopsJson;
+  walkCircles.nyc = addWalkCircles('nyc');
+  stationPiesD3.nyc = addStationPies('nyc');
+  makePies(stationPiesD3.nyc);
+  updateWalkingCircles();
+});
+
+function findNearestCity() {
+  if (marker.getLatLng().distanceTo(L.latLng(centers.nyc)) < mm(100)) {
+    city = 'nyc';
+  } else {
+    city = 'dc'
+  }
 }
 
 // This happens as soon as the distance slider is moved
@@ -106,63 +133,53 @@ function distChange(){
 map.on('click', onMapClick);
 function onMapClick(e) {
   marker.setLatLng(e.latlng);
-  onDrag();
+  markerCircle.setLatLng(marker.getLatLng());
   updateWalkingCircles();
 }
 
-function onDrag() {
+function onMarkerDrag() {
   markerCircle.setLatLng(marker.getLatLng());
-  //updateWalkingCircles();
 }
 
 const visited = [];
 const walkFromLine = []; 
 function updateWalkingCircles() {
+  findNearestCity();
   walkFromLine.fill(0);
   while (visited.length) {
     visited.pop().setRadius(0);
   }
     
-  walkCircles.forEach((o, i) => {
-  
+  walkCircles[city].forEach((o, i) => {
     if ( o.getLatLng().distanceTo(marker.getLatLng()) <= willWalk ) {
-      stops[i].servesIndex.forEach(l => {
+      stops[city][i].servesIndex.forEach(l => {
         walkFromLine[l] = Math.max(
           walkFromLine[l] || 0, 
           willWalk - o.getLatLng().distanceTo(marker.getLatLng())
         )
       });
-    }
-    
+    } 
   })
   
-  walkFromLine[0]=0;
-  walkCircles.forEach((o, i) => {
-    const newWalkRadius = Math.max(...stops[i].servesIndex.map(l => walkFromLine[l] || 0));
-    
+  walkCircles[city].forEach((o, i) => {
+    const r = Math.max(...stops[city][i].servesIndex.map(l => walkFromLine[l] || 0));
     visited.push(o);
-    o.setRadius(newWalkRadius);
-  })
-  
+    o.setRadius(r);
+  }) 
 }
 
 map.on('zoomend', onZoom);
-const zoomLookup = ['20%', '20%','20%','20%','20%','20%','20%','20%','20%',
-  '20%','20%','20%','20%','40%','40%','40%','40%','100%','100%']
+const zoomLookup = ['30%', '30%','30%','30%','30%','30%','30%','30%','30%',
+  '30%','30%','30%','30%','60%','60%','60%','60%','100%','100%']
 function onZoom(e) {
-  stationPiesD3.attr('width', zoomLookup[map.getZoom()])
+  stationPiesD3[city].attr('width', zoomLookup[map.getZoom()])
 }
 
-function helpClick(){
-  document.getElementById('helpDiv').style.display = 'none';
-  if (document.getElementById('helpCheckbox').checked) {
-    document.getElementById('helpDiv').style.display = 'block';
-  }
-}
-
-function gotItClick() {
-  document.getElementById('helpCheckbox').checked = false;
-  document.getElementById('helpDiv').style.display = 'none';
+let aboutVisible = false;
+function toggleAbout() {
+  aboutVisible = !aboutVisible;
+  document.getElementById('about-div').style.display = 
+    aboutVisible ? 'block' : 'none';
 }
 
 function mm(miles) {
